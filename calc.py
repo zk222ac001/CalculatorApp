8bea84be91f4721326e933b137605b1756275c4d
Zuhair Haroon Khan <zk222ac@gmail.com>
2026-02-23 09:30:58 +0100

    multiplication method

--- calc.py
+++ calc.py
@@ -6,7 +6,10 @@ def addition(a , b):
 def substraction(a,b):
     return a- b
 
+def Multiplication(a,b):
+    return a * b
 
 
 print(addition(2,5))
 print(substraction(5,3))
+print(Multiplication(5,5))
